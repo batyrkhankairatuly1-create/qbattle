097bb3426b476de2bc4cd88a52d2cdbf705eb874
Batyrkhan <batyrkhankairatuly1@gmail.com>
2026-05-02 22:27:53 +0500

    update songs

--- battle/management/commands/seed_songs.py
+++ battle/management/commands/seed_songs.py
@@ -8,7 +8,7 @@ SONGS = [
     {"title": "How You Like That", "artist": "BLACKPINK", "genre": "kpop", "youtube_video_id": "ioNng23DkIM"},
     {"title": "Butter", "artist": "BTS", "genre": "kpop", "youtube_video_id": "WMweEpGlu_U"},
     {"title": "Drama", "artist": "aespa", "genre": "kpop", "youtube_video_id": "D8VEhcPeSlc"},
-    {"title": "TOMBOY", "artist": "(G)I-DLE", "genre": "kpop", "youtube_video_id": "W7wqMnNSFKg"},
+    {"title": "Seven", "artist": "JungKook", "genre": "kpop", "youtube_video_id": "QU9c0053UAU"},
     {"title": "Cupid", "artist": "FIFTY FIFTY", "genre": "kpop", "youtube_video_id": "Qc7_zRjH808"},
     {"title": "New Jeans", "artist": "NewJeans", "genre": "kpop", "youtube_video_id": "G8GEpK7YDl4"},
     {"title": "Tokyo", "artist": "Moonlight", "genre": "qpop", "youtube_video_id": "EDDakaNT9aU"},
@@ -17,8 +17,8 @@ SONGS = [
     {"title": "Bir Suraq", "artist": "Zhanar Dugalova", "genre": "qpop", "youtube_video_id": "KjxGp29HEpo"},
     {"title": "Kaytadan", "artist": "Ninety One", "genre": "qpop", "youtube_video_id": "BsvKI19c3Oc"},
     {"title": "Ozin Gana", "artist": "Moldanazar", "genre": "qpop", "youtube_video_id": "j2Sx1fewNcs"},
-    {"title": "Japyraqtai", "artist": "Skazka", "genre": "qpop", "youtube_video_id": "4hVXEPnkr6E"},
-    {"title": "Tugan Zher", "artist": "Yerzhan Maxim", "genre": "qpop", "youtube_video_id": "V2xSfzIKXuA"},
+    {"title": "Sendei Aru Zhok", "artist": "Argonya", "genre": "qpop", "youtube_video_id": "tPgA5moJisA"},
+    {"title": "Aiyptama", "artist": "Ninety One", "genre": "qpop", "youtube_video_id": "A9QIgf50sh4"},
 ]
 
 
