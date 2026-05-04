a7793c85304135eaad8d701dff82607d62774c60
Batyrkhan <batyrkhankairatuly1@gmail.com>
2026-05-04 15:46:24 +0500

    add admin creation endpoint

--- battle/management/commands/create_admin.py
+++ battle/management/commands/create_admin.py
@@ -10,6 +10,5 @@ class Command(BaseCommand):
             User.objects.create_superuser("admin", "admin@example.com", "admin123")
             self.stdout.write(self.style.SUCCESS("Admin user created"))
         else:
-            self.stdout.write(self.style.WARNING("Admin user already exists"))
             User.objects.create_superuser("admin", "admin@example.com", "admin123")
             self.stdout.write(self.style.SUCCESS("Admin user created"))
